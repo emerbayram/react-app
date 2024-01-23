35d32f8a1a5de88281c59df7f4b100dba607a77d
bayramemer <bayramemer35@gmail.com>
2024-01-23 21:47:53 +0300

    Component use(button,listgroup etc)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import ListGroup from "./components/ListGroup";
-
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
 function App() {
-  let items = ["İstanbul", "Ankara", "İzmir", "Afyon", "Denizli"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        MyButton
+      </Button>
     </div>
   );
 }
